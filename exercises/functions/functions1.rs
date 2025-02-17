7631f68334e38ba127c70c2092e7353980026649
Dejun Yuan <yuandj1972@163.com>
2025-02-17 03:31:50 +0000

    终于完成作业2

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,7 +3,11 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+fn call_me() {
+    println!("Called the function");
+}
 
 fn main() {
     call_me();
